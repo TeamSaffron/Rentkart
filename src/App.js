aeaeeb8e137dd25796fc167e6e3bfe4a41575436
Gaurav Pratap Singh <gps2001.aljn@gmail.com>
2022-05-14 22:52:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import './styles/index.scss';
+import Rentkart from './Component/Rentkart';
+
+function App() {
+  const onClickHandelere=()=>{
+    
+  }
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Rentkart />
+    //   </header>
+    // </div>
+    <Rentkart >
+    </Rentkart>
+  );
+}
+
+export default App;
