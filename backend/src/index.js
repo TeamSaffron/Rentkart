ba39dd16989cfe75f9c80d038ac2b5af6b57426a
Ashish Singh <ashishsingh07.work@gmail.com>
2022-05-15 16:44:46 +0530

    created CRUDc login and logout APIs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,66 +1,90 @@
-import express, { Router, json } from 'express';
-import Room from "./models/room.js";
-import './db/mongoose.js';
-// import User from './models/user';
+import express, { Router, json } from "express";
+const app = express();
+import auth from './middleware/auth.js'
+import multer from 'multer'
+import sharp from 'sharp'
+import "./db/mongoose.js";
+import User from "./models/user.js";
+const port = process.env.PORT || 3001;
 
-const app = express()
-const port = process.env.PORT || 3001
+app.use(json());
 
+app.post("/users", async (req, res) => {
+  const user = new User(req.body);
 
-app.use(json())
+  try {
+    await user.save();
+    const token = await user.generateAuthToken()
+    res.status(201).send({user, token});
+  } catch (e) {
+    res.status(400).send(e);
+    console.log(e);
+  }
+});
 
-// app.post('/user', (req, res) => {
-//     const landlord = new User(req.body)
+app.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken()
+    res.send({user, token});
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-//     landlord.save().then(() => {
-//         res.status(201).send(landlord)
-//     }).catch((e) => {
-//         res.status(400).send(e)
-//     })
-// })
+app.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
 
-// app.post('/Tenants', (req, res) => {
-//     const tenant = new User(req.body)
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-//     tenant.save().then(() => {
-//         res.status(201).send(tenant)
-//     }).catch((e) => {
-//         res.status(400).send(e)
-//     })
-// })
+app.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
 
-// app.get('/users', (req, res) => {
-//     User.find({}).then((users) => {
-//         res.send(users)
-//     }).catch((e) => {
-//         res.status(500).send()
-//     })
-// })
+app.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-// app.get('/users/:id', (req, res) => {
-//     const _id = req.params.id
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
 
-//     User.findById(_id).then((user) => {
-//         if (!user) {
-//             return res.status(404).send()
-//         }
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
 
-//         res.send(user)
-//     }).catch((e) => {
-//         res.status(500).send()
-//     })
-// })
+    if (!req.user) {
+      return res.status(404).send();
+    }
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-// app.post('/tasks', (req, res) => {
-//     const task = new Task(req.body)
-
-//     task.save().then(() => {
-//         res.status(201).send(task)
-//     }).catch((e) => {
-//         res.status(400).send(e)
-//     })
-// })
+app.delete("/users/me", auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
